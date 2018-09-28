4e7af219d3bbb55788ea68f301fc2226cd100461
Brandon Kim <bkim2490@gmail.com>
2018-09-28 23:37:28 +0900

    styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -21,7 +21,12 @@ $('input[type=\'text\']').keypress(function(event){
             let todoText = $(this).val();
             $(this).val('');
             // create a new li and add to ul
-            $('ul').append('<li><span>X</span> ' + todoText + '</li>');
+            $('ul').append('<li><span><i class="fa fa-trash"></i></span> ' + todoText + '</li>');
         }
     }
-})
+});
+
+// Toggle add form
+$('#plus').click(function(){
+    $('input[type=\'text\']').fadeToggle();
+});
